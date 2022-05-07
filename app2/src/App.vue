b7939d967aa4a4672539717ab2377cc43bac7cf0
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2022-05-07 14:25:11 +0700

    1
    1

---
+++ app2/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+1
+</template>
+<script>
+export default {
+  name: "App",
+
+  data() {
+    return {
+      picture:"https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      user: "001",
+      nick: "kaw",
+
+    };
+  },
+
+  methods: {
+    togglevisible() {
+      this.isvisible = !this.isvisible;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+}
+</style>
