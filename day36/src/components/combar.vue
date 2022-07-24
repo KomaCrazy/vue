262b7da84f3d7add15f33735af0c74d6d43a9fb3
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2022-07-24 22:09:15 +0700

    ๅ

---
+++ day36/src/components/combar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+export default{
+    name:"combar",
+}
+</script>
---
+++ day36/src/views/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+        <combar/>
+
+        
+    </div>
+</template>
+
+<script>
+import combar from '../components/combar.vue'
+
+export default{
+  components: { combar },
+    name:"Test",
+    comments:{
+        combar,
+    }
+}
+</script>
